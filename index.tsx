10058e59335b52287081c874180cc83e974737a4
qwelqap <qweqaplqapl@gmail.com>
2025-12-22 22:13:26 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -18,7 +18,7 @@ root.render(
 // Register Service Worker for PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/sw.js', { scope: '/' })
       .then(registration => {
         console.log('Service Worker registered:', registration);
       })
